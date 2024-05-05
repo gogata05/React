719a002107645292d9df4ae21cd16e7389fb0bf1
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-05 05:37:41 +0300

    open modal popup with X

--- 01.Components/01. 25 React Components/src/App.js
+++ 01.Components/01. 25 React Components/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import ModalTest from "./components/custom-modal-popup/modal-test";
 
 function App() {
   return (
     <div className="App">
       {/* Accordion component */}
-      <Accordion/>
+      {/* <Accordion/> */}
+            {/* Custom Modal Component */}
+      <ModalTest/>
 
     </div>
   );
