4d710ae4c74d4b86513ae3146d2b3d422a675faa
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-06 03:14:40 +0300

    LightDarkMode

--- 01.Components/01. All React Components/src/App.js
+++ 01.Components/01. All React Components/src/App.js
@@ -4,6 +4,8 @@ import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
 import GithubProfileFinder from "./components/github-profile-finder";
 import ImageSlider from "./components/image-slider";
+import LightDarkMode from "./components/light-dark-mode";
+
 function App() {
   return (
     <div className="App">
@@ -20,11 +22,13 @@ function App() {
       {/* <GithubProfileFinder /> */}
 
       {/* Image slider components with arrows */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      {/* light and dark theme switch */}
+      <LightDarkMode />
     </div>
   );
 }
