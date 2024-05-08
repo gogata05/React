fc8aaaeed090283e0d65d93d2cb62dfad9dea607
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-08 06:36:23 +0300

    use windows resize

--- 01.Components/01. All React Components/src/App.js
+++ 01.Components/01. All React Components/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Accordion from "./components/accordion";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
+import FeatureFlags from "./components/feature-flag";
+import FeatureFlagGlobalState from "./components/feature-flag/context";
 import GithubProfileFinder from "./components/github-profile-finder";
 import ImageSlider from "./components/image-slider";
 import LightDarkMode from "./components/light-dark-mode";
@@ -18,20 +20,18 @@ import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 import UseFetchHookTest from "./components/use-fetch/test";
 import UseOnclickOutsideTest from "./components/use-outside-click/test";
+import UseWindowResizeTest from "./components/use-window-resize/test";
+
 function App() {
   return (
     <div className="App">
       {/* Accordion component */}
       {/* <Accordion/> */}
+      {/* Random color component */}
+      {/* <RandomColor/> */}
 
-      {/* Custom Modal Component */}
-      {/* <ModalTest/> */}
-
-      {/* Custom tabs component */}
-      {/* <TabTest /> */}
-
-      {/* Github profile finder */}
-      {/* <GithubProfileFinder /> */}
+      {/* Star rating component */}
+      {/* <StarRating noOfStars={5}/> */}
 
       {/* Image slider components with arrows */}
       {/* <ImageSlider
@@ -40,44 +40,55 @@ function App() {
         limit={"10"}
       /> */}
 
-      {/* light and dark theme switch */}
-      {/* <LightDarkMode /> */}
-
       {/* Load more products component */}
-      {/* <LoadMoreData /> */}
+      {/* <LoadMoreData/> */}
+
+      {/*Left Tree navigation menu- Tree view component/menu UI component */}
+      {/* <TreeView menus={menus} /> */}
 
       {/* QR code generator */}
-      {/* <QRCodeGenerator /> */}
+      {/* <QRCodeGenerator/> */}
 
-      {/* Random color component */}
-      {/* <RandomColor/> */}
+      {/* light and dark theme switch */}
+      {/* <LightDarkMode/> */}
 
       {/* Scroll indicator component */}
       {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
+
+      {/* Custom tabs component */}
+      {/* <TabTest /> */}
+
+      {/* Custom Modal Component */}
+      {/* <ModalTest/> */}
+
+      {/* Github profile finder */}
+      {/* <GithubProfileFinder/> */}
+
+      {/* Search Autocomplete */}
+      {/* <SearchAutocomplete/> */}
+
+      {/* Tic tac toe */}
+      {/* <TicTacToe/> */}
+
+      {/* Feature Flag IMplementation */}
+      {/* <FeatureFlagGlobalState>
+        <FeatureFlags />
+      </FeatureFlagGlobalState> */}
+
+      {/* useFetch - Custom Hook Test*/}
+      {/* <UseFetchHookTest/> */}
+
+      {/* Use Onclick Outside Hook Test */}
+      {/* <UseOnclickOutsideTest/> */}
+
+      {/* Windows Size */}
+      {/* <UseWindowResizeTest/> */}
 
       {/* Scroll to Top and Bottom */}
       {/* <ScrollToTopAndBottom/> */}
 
       {/* Scroll to a Particular Section */}
       {/* <ScrollToSection/> */}
-
-      {/* Search Autocomplete */}
-      {/* <SearchAutocomplete /> */}
-
-      {/* Star rating component */}
-      {/* <StarRating noOfStars={5} /> */}
-
-      {/* Tic tac toe */}
-      {/* <TicTacToe /> */}
-
-      {/*Left Tree navigation menu- Tree view component/menu UI component */}
-      {/* <TreeView menus={menus} /> */}
-
-      {/* useFetch - Custom Hook Test*/}
-      {/* <UseFetchHookTest /> */}
-
-      {/* Use Onclick Outside Hook Test */}
-      <UseOnclickOutsideTest />
     </div>
   );
 }
